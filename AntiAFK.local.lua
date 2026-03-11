c293fd2a59820dea52e3b3f3f38fde2e165ffc74
Shiro <daniel@mikro.ninja>
2026-03-11 18:19:43 +0200

    Add AntiAFK script to prevent player idling
    This script prevents the player from going idle by simulating a button press.

---
+++ AntiAFK.local.lua
@@ -0,0 +1,16 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local VirtualUser = game:GetService("VirtualUser")
+
+local currentCamera = game.Workspace.CurrentCamera
+
+player.Idled:Connect(function()
+	VirtualUser:Button2Down(Vector2.zero, currentCamera.CFrame)
+	task.wait(1)
+	VirtualUser:Button2Up(Vector2.zero, currentCamera.CFrame)
+	
+	print("Player Successfully UnIdled.")
+end)
+
+print("Anti AFK Active")
